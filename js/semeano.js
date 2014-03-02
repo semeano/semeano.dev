7a624185c7c57657f68b174a3155764b6c2de1a2
Pedro Semeano <semeano@gmail.com>
2014-03-02 23:41:20 +0000

    Icons changed to font icons.

--- js/semeano.js
+++ js/semeano.js
@@ -8,22 +8,3 @@ function page_loaded() {
     // Show page
     document.body.style.opacity = "1";
 }
-
-
-
-/*
- * Contact images
- *
- */
-
-$(function() {
-  $('.contact img')
-    .mouseover(function() { 
-      var src = $(this).attr('src').match(/[^\.]+/) + '_hover.png';
-      $(this).attr('src', src);
-    })
-    .mouseout(function() {
-      var src = $(this).attr('src').replace('_hover.png', '.png');
-      $(this).attr('src', src);
-    });
-});
