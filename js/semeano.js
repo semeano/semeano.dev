8cf7632d28ff54163d00c188328207bf5ce5333b
Pedro Semeano <semeano@gmail.com>
2014-03-02 19:03:14 +0000

    Cleanup

--- js/semeano.js
+++ js/semeano.js
@@ -5,37 +5,10 @@
 
 function page_loaded() {
 
-    //set_up_panel_sizes();
-
     // Show page
     document.body.style.opacity = "1";
 }
 
-
-/*
- * Sections resize
- *
- */
-
-/*$(window).resize(function() {
-    set_up_panel_sizes();
-});*/
-
-function set_up_panel_sizes() {
-
-    var window_height = $(window).height();
-
-    // Set section's height
-    $('.section').css('height', window_height);
-    
-    // Set section content's padding top
-    var section_content = $('.section_content');
-    for (var i = 0; i < section_content.length; i++) {
-        var height = parseInt($($('.section_content')[i]).css('height').replace('px', ''));
-        var padding_top = (window_height - height) / 2.2;
-        $($('.section_content')[i]).css('padding-top', padding_top);
-    }
-}
 
 
 /*
